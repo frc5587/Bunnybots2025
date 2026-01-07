d0af8f921d7d02a452d4635399700dafcfed46c2
titanrobotics5587 <technical@frc5587.org>
2026-01-07 18:05:13 -0500

    Changed the time function in RotateToHeading

--- src/main/java/frc/robot/commands/RotateToHeading.java
+++ src/main/java/frc/robot/commands/RotateToHeading.java
@@ -1,9 +1,8 @@
 package frc.robot.commands;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.function.DoubleSupplier;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.SwerveSubsystem;
 
@@ -17,8 +16,7 @@ public class RotateToHeading extends Command {
   private final double secondsWithinMargin;
 
   private double targetHeading;
-  
-  private Instant lastInstantOutOfRange;
+  private double lastTimeOutOfRange;
   
   public RotateToHeading(SwerveSubsystem _swerve, DoubleSupplier _headingSupplier, double _errorMargin, double _secondsWithinMargin) {
     swerve = _swerve;
@@ -31,20 +29,19 @@ public class RotateToHeading extends Command {
   public void initialize() {
     targetHeading = headingSupplier.getAsDouble();
     swerve.overrideHeading(targetHeading, this);
-    lastInstantOutOfRange = Instant.now();
+    lastTimeOutOfRange = Timer.getFPGATimestamp();
   }
 
   @Override
   public void execute() {
     if (Math.abs(swerve.getHeading().getDegrees() - targetHeading) > errorMargin) {
-      lastInstantOutOfRange = Instant.now();
+      lastTimeOutOfRange = Timer.getFPGATimestamp();
     }
   }
 
   @Override
   public boolean isFinished() {
-    Duration timeElapsed = Duration.between(lastInstantOutOfRange, Instant.now());
-    double secondsElapsed = timeElapsed.toNanos()/1000000000.0;
+    double secondsElapsed = Timer.getFPGATimestamp() - lastTimeOutOfRange;
 
     double currentHeading = swerve.getHeading().getDegrees();
     boolean isInRange = Math.abs(currentHeading - targetHeading) < errorMargin;
